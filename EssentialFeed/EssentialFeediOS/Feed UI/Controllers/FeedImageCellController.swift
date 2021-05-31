341533465e0a89746f5bff68ca6d5f974da7e3d6
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-05-30 21:11:14 -0500

    Move CellController dependencies from shared module.

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedImageCellController.swift
@@ -10,7 +10,7 @@ public protocol FeedImageCellControllerDelegate {
 	func didCancelImageRequest()
 }
 
-public final class FeedImageCellController: CellController, ResourceView, ResourceLoadingView, ResourceErrorView {
+public final class FeedImageCellController: NSObject, CellController, ResourceView, ResourceLoadingView, ResourceErrorView {
 	public typealias ResourceViewModel = UIImage
 	private let viewModel: FeedImageViewModel
 	private let delegate: FeedImageCellControllerDelegate
@@ -21,7 +21,13 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
 		self.delegate = delegate
 	}
 	
-	public func view(in tableView: UITableView) -> UITableViewCell {
+	
+	
+	public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		1
+	}
+	
+	public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		cell = tableView.dequeueReusableCell()
 		cell?.locationContainer.isHidden = !viewModel.hasLocation
 		cell?.locationLabel.text = viewModel.location
@@ -31,11 +37,19 @@ public final class FeedImageCellController: CellController, ResourceView, Resour
 		return cell!
 	}
 	
-	public func preload() {
+	public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+		cancelLoad()
+	}
+	
+	public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
 		delegate.didRequestImage()
 	}
 	
-	public func cancelLoad() {
+	public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
+		cancelLoad()
+	}
+	
+	private func cancelLoad() {
 		releaseCellForReuse()
 		delegate.didCancelImageRequest()
 	}

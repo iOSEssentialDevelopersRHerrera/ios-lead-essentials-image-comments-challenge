cec2370936b8e211fc92b52515f9708664d4e3a9
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-05-31 21:43:02 -0500

    Changing Error View to UIButton messes up the Snapshot tests.

--- EssentialFeed/EssentialFeediOS/Shared UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Views/ErrorView.swift
@@ -4,18 +4,11 @@
 
 import UIKit
 
-public final class ErrorView: UIView {
-	private lazy var label: UILabel = {
-		let label = UILabel()
-		label.textColor = .white
-		label.textAlignment = .center
-		label.numberOfLines = 0
-		label.font = .systemFont(ofSize: 17)
-		return label
-	}()
+public final class ErrorView: UIButton {
+
 	
 	public var message: String? {
-		get { return isVisible ? label.text : nil }
+		get { return isVisible ? title(for: .normal) : nil }
 		set { setMessageAnimated(newValue) }
 	}
 	
@@ -31,20 +24,17 @@ public final class ErrorView: UIView {
 	private func configure() {
 		backgroundColor = .errorBackgroundColor
 		
+		addTarget(self, action: #selector(hideMessageAnimated), for: .touchUpInside)
 		configureLabel()
 		hideMessage()
 	}
 	
 	private func configureLabel() {
-		addSubview(label)
-		label.translatesAutoresizingMaskIntoConstraints = false
-		NSLayoutConstraint.activate([
-			label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
-			trailingAnchor.constraint(equalTo: label.trailingAnchor, constant: 8),
-			label.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-			bottomAnchor.constraint(equalTo: label.bottomAnchor, constant: 8)
-			
-		])
+		titleLabel?.textColor = .white
+		titleLabel?.textAlignment = .center
+		titleLabel?.numberOfLines = 0
+		titleLabel?.font = .systemFont(ofSize: 17)
+		
 	}
 	
 	public override func awakeFromNib() {
@@ -66,7 +56,8 @@ public final class ErrorView: UIView {
 	}
 	
 	private func showAnimated(_ message: String) {
-		label.text = message
+		setTitle(message, for: .normal)
+		contentEdgeInsets = .init(top: 8, left: 8, bottom: 8, right: 8)
 		
 		UIView.animate(withDuration: 0.25) {
 			self.alpha = 1
@@ -83,8 +74,9 @@ public final class ErrorView: UIView {
 	}
 	
 	private func hideMessage() {
-		label.text = nil
+		setTitle(nil, for: .normal)
 		alpha = 0
+		contentEdgeInsets = .init(top: -2.5, left: 0, bottom: -2.5, right: 0)
 	}
 	
 }

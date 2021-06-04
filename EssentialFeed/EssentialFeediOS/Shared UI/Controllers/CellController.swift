a906f6098ca57ba9bc4924945c6574fd307b576e
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-06-03 20:50:05 -0500

    Use diffable data source to only reload cells when needed

--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/CellController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/CellController.swift
@@ -9,19 +9,33 @@
 import UIKit
 
 public struct CellController  {
+	let id: AnyHashable
 	let dataSource: UITableViewDataSource
 	let delegate: UITableViewDelegate?
 	let datasourcePrefecthing: UITableViewDataSourcePrefetching?
 	
-	public init(_ dataSource: UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching) {
+	public init(id: AnyHashable, _ dataSource: UITableViewDataSource & UITableViewDelegate & UITableViewDataSourcePrefetching) {
+		self.id = id
 		self.dataSource = dataSource
 		self.delegate = dataSource
 		self.datasourcePrefecthing = dataSource
 	}
 	
-	public init(_ dataSource: UITableViewDataSource) {
+	public init(id:AnyHashable,  _ dataSource: UITableViewDataSource) {
+		self.id = id
 		self.dataSource = dataSource
 		self.delegate = nil
 		self.datasourcePrefecthing = nil
+	}
+}
+
+extension CellController: Equatable {
+	public static func == (lhs: CellController, rhs: CellController) -> Bool {
+		lhs.id == rhs.id
+	}
+}
+extension CellController: Hashable {
+	public func hash(into hasher: inout Hasher) {
+		hasher.combine(id)
 	}
 }

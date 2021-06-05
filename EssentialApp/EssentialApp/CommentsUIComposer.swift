0df431696ceab5f5d0c9ab0937bbef159823b3ca
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-06-04 21:19:49 -0500

    Set comments title

--- EssentialApp/EssentialApp/CommentsUIComposer.swift
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -17,7 +17,7 @@ public final class CommentsUIComposer {
 			FeedViewAdapter>(loader: commentsLoader)
 		
 		let feedController = makeFeedViewController(
-			title: FeedPresenter.title)
+			title: ImageCommentsPresenter.title)
 		feedController.onRefresh = presentationAdapter.loadResource
 		
 		presentationAdapter.presenter = LoadResourcePresenter(

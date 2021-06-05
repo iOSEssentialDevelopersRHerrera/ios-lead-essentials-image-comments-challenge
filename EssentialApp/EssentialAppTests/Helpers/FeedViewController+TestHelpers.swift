54deda50ac5cbab0c0b8deee7aa8ba2c5ce0e8f9
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2021-06-04 22:16:11 -0500

    Render comments

--- EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedViewController+TestHelpers.swift
@@ -16,6 +16,55 @@ extension ListViewController {
 		refreshControl?.simulatePullToRefresh()
 	}
 	
+	var isShowingLoadingIndicator: Bool {
+		return refreshControl?.isRefreshing == true
+	}
+	
+	func simulateErrorViewTap() {
+		errorView.simulateTap()
+	}
+	
+	var errorMessage: String? {
+		return errorView.message
+	}
+}
+
+extension ListViewController {
+	
+	func numberOfRenderedComments() -> Int {
+		tableView.numberOfSections == 0 ? 0 :
+		 tableView.numberOfRows(inSection: commentsSection)
+	}
+	
+	func commentMessage(at row: Int) -> String? {
+		commentView(at: row)?.messageLabel.text
+	}
+	
+	func commentDate(at row: Int) -> String? {
+		commentView(at: row)?.dateLabel.text
+	}
+	
+	func commentUsername(at row: Int) -> String? {
+		commentView(at: row)?.usernameLabel.text
+	}
+	
+	private func commentView(at row: Int) -> ImageCommentCell? {
+		guard numberOfRenderedComments() > row else {
+			return nil
+		}
+		let ds = tableView.dataSource
+		let index = IndexPath(row: row, section: commentsSection)
+		return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+	}
+	
+	private var commentsSection: Int {
+		return 0
+	}
+	
+}
+
+extension ListViewController {
+	
 	@discardableResult
 	func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
 		return feedImageView(at: index) as? FeedImageCell
@@ -50,23 +99,11 @@ extension ListViewController {
 		return simulateFeedImageViewVisible(at: index)?.renderedImage
 	}
 	
-	func simulateErrorViewTap() {
-		errorView.simulateTap()
-	}
-	
-	var errorMessage: String? {
-		return errorView.message
-	}
-	
-	var isShowingLoadingIndicator: Bool {
-		return refreshControl?.isRefreshing == true
-	}
-	
 	func numberOfRenderedFeedImageViews() -> Int {
 		tableView.numberOfSections == 0 ? 0 :
 		 tableView.numberOfRows(inSection: feedImagesSection)
 	}
-	
+
 	func feedImageView(at row: Int) -> UITableViewCell? {
 		guard numberOfRenderedFeedImageViews() > row else {
 			return nil
@@ -79,4 +116,6 @@ extension ListViewController {
 	private var feedImagesSection: Int {
 		return 0
 	}
+	
+	
 }
